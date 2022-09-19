71508b4648b47027ecf33f2edb86b0258e0e8a87
Damilare <bolarinwaoluwadamilare91@gmail.com>
2022-09-19 15:09:43 +0100

    done with getting all couses and display them in the right way

--- src/app/course.service.ts
+++ src/app/course.service.ts
@@ -16,6 +16,38 @@ export class CourseService {
     },
     {
       id:2,
+      text:'We are going to teach you react from basic to advance',
+      image:'../assets/react_advance.png',
+      type:"Not Free",
+      price:150,
+      title:'react for advance'
+    },
+    {
+      id:3,
+      text:'We are going to teach you javascript basic',
+      image:'../assets/js_ben.png',
+      type:"Not Free",
+      price:50,
+      title:'javascript for basic'
+    },
+    {
+      id:4,
+      text:'we are going to teach you javascript from basic to advance',
+      image:'../assets/js fo advance.png',
+      type:" not free",
+      price:350,
+      title:'javascript from basic to advance'
+    },
+    {
+      id:5,
+      text:'we are going to teach you react form basic to intermedite',
+      image:'../assets/reacct_inter.png',
+      type:"not free",
+      price:100,
+      title:'react form basic to intermedite'
+    },
+    {
+      id:6,
       text:'We are going to teach you angular basic to intermedite',
       image:'../assets/angular for intermedite.png',
       type:"Not Free",
@@ -23,44 +55,12 @@ export class CourseService {
       title:'angular for intermedite'
     },
     {
-      id:3,
-      text:'We are going to teach you angular basic to advance',
-      image:'../assets/ang for advance.png',
-      type:"Not Free",
-      price:200,
-      title:'angular for advance'
-    },
-    {
-      id:1,
-      text:'we are going to teach you angular basic',
-      image:'../assets/Angular.png',
-      type:"free",
-      price:0,
-      title:'angular for beginner'
-    },
-    {
-      id:1,
-      text:'we are going to teach you angular basic',
-      image:'../assets/Angular.png',
-      type:"free",
-      price:0,
-      title:'angular for beginner'
-    },
-    {
-      id:1,
-      text:'we are going to teach you angular basic',
-      image:'../assets/Angular.png',
-      type:"free",
-      price:0,
-      title:'angular for beginner'
-    },
-    {
-      id:1,
-      text:'we are going to teach you angular basic',
-      image:'../assets/Angular.png',
-      type:"free",
-      price:0,
-      title:'angular for beginner'
+      id:7,
+      text:'we are going to teach you react basic',
+      image:'../assets/react for new.png',
+      type:" notfree",
+      price:35,
+      title:'react basic'
     },
     {
       id:8,
@@ -71,7 +71,7 @@ export class CourseService {
       title:'angular for advance'
     }, 
     {
-      id:1,
+      id:9,
       text:'we are going to teach you angular basic',
       image:'../assets/Angular.png',
       type:"free",
